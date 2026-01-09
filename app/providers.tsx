86390bb3bb48b51daf18c0f31a289d8a7582d318
Louis Beaumont <louis.beaumont@gmail.com>
2026-01-09 15:45:34 -0800

    fix: simplify PostHog init

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,62 +1,20 @@
 'use client'
 
-import { usePathname, useSearchParams } from "next/navigation"
-import { useEffect, Suspense } from "react"
 import posthog from 'posthog-js'
-import { PostHogProvider as PHProvider, usePostHog } from 'posthog-js/react'
+import { PostHogProvider as PHProvider } from 'posthog-js/react'
+import { useEffect } from 'react'
 
-const LANDING_PAGE_NAME = 'autoehr'
+const POSTHOG_KEY = 'phc_NFSaZUao49XckpqaeyB3lIEKrFXhhXbKaI81jqZ8yn9'
+const POSTHOG_HOST = 'https://eu.i.posthog.com'
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
-    const posthogKey = process.env.NEXT_PUBLIC_POSTHOG_KEY || 'phc_NFSaZUao49XckpqaeyB3lIEKrFXhhXbKaI81jqZ8yn9';
-    const posthogHost = process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://eu.i.posthog.com';
-
-    if (posthogKey) {
-      posthog.init(posthogKey, {
-        api_host: posthogHost,
-        person_profiles: 'always',
-        capture_pageview: true,
-        capture_pageleave: true,
-      });
-      posthog.register({ landing_page: LANDING_PAGE_NAME });
-    }
+    posthog.init(POSTHOG_KEY, {
+      api_host: POSTHOG_HOST,
+      capture_pageview: true,
+      capture_pageleave: true,
+    })
   }, [])
 
-  return (
-    <PHProvider client={posthog}>
-      <SuspendedPostHogPageView />
-      {children}
-    </PHProvider>
-  )
-}
-
-function PostHogPageView() {
-  const pathname = usePathname()
-  const searchParams = useSearchParams()
-  const posthogInstance = usePostHog()
-
-  useEffect(() => {
-    if (pathname && posthogInstance) {
-      let url = window.origin + pathname
-      const searchParamsString = searchParams?.toString() || "";
-      if (searchParamsString) {
-        url = url + "?" + searchParamsString;
-      }
-      posthogInstance.capture('$pageview', {
-        '$current_url': url,
-        landing_page: LANDING_PAGE_NAME
-      });
-    }
-  }, [pathname, searchParams, posthogInstance])
-
-  return null
-}
-
-function SuspendedPostHogPageView() {
-  return (
-    <Suspense fallback={null}>
-      <PostHogPageView />
-    </Suspense>
-  )
+  return <PHProvider client={posthog}>{children}</PHProvider>
 }
